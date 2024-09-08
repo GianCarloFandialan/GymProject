dbc45125090ed8f92d063c0558cca4a9e0d6238b
Gian Carlo Fandialan <gianchifandialan03@gmail.com>
2024-09-08 22:05:45 +0200

    Aggiornato

--- frontend/src/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import App from "./App";
-import { expect, test } from "vitest";
-
-test("Verifica che il componente homepage venga montato correttamente", () => {
-  render(<App />);
-  const homepageComponent = screen.getByTestId("homepage");
-  expect(homepageComponent).toBeInTheDocument();
-});
